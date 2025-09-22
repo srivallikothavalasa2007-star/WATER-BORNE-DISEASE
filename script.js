0490a1eba2a4d6f4140a40778338065d8da95f33
Kothavalasa Gouridurga srivalli <srivallikothavalasa2007@gmail.com>
2025-09-22 23:41:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleExtra() {
+  const extra = document.getElementById("extraInfo");
+  if (extra.style.display === "none") {
+    extra.style.display = "block";
+  } else {
+    extra.style.display = "none";
+  }
+}
